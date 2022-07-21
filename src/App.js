4ec6bdcf1b5bd893e361a2a493f116d2a8be0052
RaminiGouriPriya <gouriraghavi@gmail.com>
2022-07-22 01:36:46 +0530

    Implement all state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,153 @@
+import { useEffect, useState } from "react";
 import { Container } from "semantic-ui-react";
 import "./App.css";
 import DisplayBalance from "./components/DisplayBalance";
 import DisplayBalances from "./components/DisplayBalances";
-import EntryLine from "./components/EntryLine";
+import EntryLines from "./components/EntryLines";
 import MainHeader from "./components/MainHeader";
+import ModalEdit from "./components/ModalEdit";
 import NewEntryForm from "./components/NewEntryForm";
 
 function App() {
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+  const [isExpense, setIsExpense] = useState(true);
+  const [entries, setEntries] = useState(initialEntries);
+  const [isOpen, setIsOpen] = useState(false);
+  const [entryId, setEntryId] = useState();
+  const [incomeTotal, setIncomeTotal] = useState(0);
+  const [expenseTotal, setExpenseTotal] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    if (!isOpen && entryId) {
+      const index = entries.findIndex((entry) => entry.id === entryId);
+      const newEntries = [...entries];
+      newEntries[index].description = description;
+      newEntries[index].value = value;
+      newEntries[index].isExpense = isExpense;
+      setEntries(newEntries);
+      resetEntry();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isOpen]);
+
+  useEffect(() => {
+    let totalIncomes = 0;
+    let totalExpenses = 0;
+    entries.map((entry) => {
+      if (entry.isExpense) {
+        return (totalExpenses += Number(entry.value));
+      } else {
+        return (totalIncomes += Number(entry.value));
+      }
+    });
+    setTotal(totalIncomes - totalExpenses);
+    setExpenseTotal(totalExpenses);
+    setIncomeTotal(totalIncomes);
+  }, [entries]);
+
+  const deleteEntry = (id) => {
+    const result = entries.filter((entry) => entry.id !== id);
+    setEntries(result);
+  };
+
+  const addEntry = () => {
+    const result = entries.concat({
+      id: entries.length + 1,
+      description,
+      value,
+      isExpense,
+    });
+    setEntries(result);
+  };
+
+  const editEntry = (id) => {
+    console.log(`edit entry with is ${id}`);
+    if (id) {
+      const index = entries.findIndex((entry) => entry.id === id);
+      const entry = entries[index];
+      setEntryId(id);
+      setDescription(entry.description);
+      setValue(entry.value);
+      setIsExpense(entry.isExpense);
+      setIsOpen(true);
+    }
+  };
+
+  const resetEntry = () => {
+    setDescription("");
+    setValue("");
+    setIsExpense(true);
+  };
+
   return (
     <Container>
       <MainHeader title="Budget" />
 
-      <DisplayBalance title={"Your Balance"} value="2,550.53" size="small" />
+      <DisplayBalance title={"Your Balance"} value={total} size="small" />
 
-      <DisplayBalances />
+      <DisplayBalances incomeTotal={incomeTotal} expenseTotal={expenseTotal} />
 
       <MainHeader title="History" type="h3" />
 
-      <EntryLine description="income" value="$10.00" />
-      <EntryLine description="expense" value="$10.00" isExpense />
+      <EntryLines
+        entries={entries}
+        deleteEntry={deleteEntry}
+        editEntry={editEntry}
+      />
 
       <MainHeader type="h3" title="Add new transaction" />
 
-      <NewEntryForm />
+      <NewEntryForm
+        addEntry={addEntry}
+        description={description}
+        value={value}
+        isExpense={isExpense}
+        setDescription={setDescription}
+        setValue={setValue}
+        setIsExpense={setIsExpense}
+      />
+      <ModalEdit
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        addEntry={addEntry}
+        description={description}
+        value={value}
+        isExpense={isExpense}
+        setDescription={setDescription}
+        setValue={setValue}
+        setIsExpense={setIsExpense}
+      />
     </Container>
   );
 }
 
 export default App;
+
+var initialEntries = [
+  {
+    id: 1,
+    description: "Work Income",
+    value: 1000.0,
+    isExpense: false,
+  },
+  {
+    id: 2,
+    description: "Water bill",
+    value: 20.0,
+    isExpense: true,
+  },
+  {
+    id: 3,
+    description: "Rent",
+    value: 300,
+    isExpense: true,
+  },
+  {
+    id: 4,
+    description: "Power Bill",
+    value: 50.0,
+    isExpense: true,
+  },
+];
